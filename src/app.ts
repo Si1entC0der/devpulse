a9196d928ae885121d95afd745a58ce28676fada
Si1entC0der <shamim.sust.cse.32@gmail.com>
2026-05-23 13:53:45 +0600

    add application routes for authentication and issue management

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import { AuthRoutes } from "./modules/auth/auth.route";
+import { IssueRoutes } from "./modules/issues/issue.route";
 import globalErrorHandler from "./middleware/globalErrorHandler";
 import notFound from "./middleware/notFound";
 
@@ -8,6 +10,9 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
+// application routes
+app.use("/api/auth", AuthRoutes);
+app.use("/api/issues", IssueRoutes);
 
 const testRoute = (req: Request, res: Response) => {
   res.send("DevPulse API is running");
